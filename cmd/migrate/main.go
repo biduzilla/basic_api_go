fc46f27fc2630331b95108b77cf1dc169294aa2d
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-06-11 08:37:27 -0300

    update docker compose

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -4,12 +4,11 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"myapp/config"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
-
-	"myapp/config"
 )
 
 const (
@@ -52,3 +51,30 @@ func main() {
 		log.Fatalf("migrate %v: %v", command, err)
 	}
 }
+
+func usage() {
+	fmt.Println(usagePrefix)
+	flags.PrintDefaults()
+	fmt.Println(usageCommands)
+}
+
+var (
+	usagePrefix = `Usage: migrate COMMAND
+Examples:
+    migrate status
+`
+
+	usageCommands = `
+Commands:
+    up                   Migrate the DB to the most recent version available
+    up-by-one            Migrate the DB up by 1
+    up-to VERSION        Migrate the DB to a specific VERSION
+    down                 Roll back the version by 1
+    down-to VERSION      Roll back to a specific VERSION
+    redo                 Re-run the latest migration
+    reset                Roll back all migrations
+    status               Dump the migration status for the current DB
+    version              Print the current version of the database
+    create NAME [sql|go] Creates new migration file with the current timestamp
+    fix                  Apply sequential ordering to migrations`
+)
