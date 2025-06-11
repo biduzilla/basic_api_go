8bb010904fbcc02a4ea4f348acb64ecaa80416bc
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2025-06-11 08:32:50 -0300

    config env

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -8,11 +8,13 @@ import (
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/pressly/goose/v3"
+
+	"myapp/config"
 )
 
 const (
-	dialect  = "pgx"
-	dbString = "host=localhost user=myapp_user password=myapp_pass dbname=myapp_db port=5432 sslmode=disable"
+	dialect     = "pgx"
+	fmtDBString = "host=%s user=%s password=%s dbname=%s port=%d sslmode=disable"
 )
 
 var (
@@ -32,6 +34,9 @@ func main() {
 
 	command := args[0]
 
+	c := config.NewDB()
+	dbString := fmt.Sprintf(fmtDBString, c.Host, c.Username, c.Password, c.DBName, c.Port)
+
 	db, err := goose.OpenDBWithDriver(dialect, dbString)
 	if err != nil {
 		log.Fatalf(err.Error())
@@ -47,30 +52,3 @@ func main() {
 		log.Fatalf("migrate %v: %v", command, err)
 	}
 }
-
-func usage() {
-	fmt.Println(usagePrefix)
-	flags.PrintDefaults()
-	fmt.Println(usageCommands)
-}
-
-var (
-	usagePrefix = `Usage: migrate COMMAND
-Examples:
-    migrate status
-`
-
-	usageCommands = `
-Commands:
-    up                   Migrate the DB to the most recent version available
-    up-by-one            Migrate the DB up by 1
-    up-to VERSION        Migrate the DB to a specific VERSION
-    down                 Roll back the version by 1
-    down-to VERSION      Roll back to a specific VERSION
-    redo                 Re-run the latest migration
-    reset                Roll back all migrations
-    status               Dump the migration status for the current DB
-    version              Print the current version of the database
-    create NAME [sql|go] Creates new migration file with the current timestamp
-    fix                  Apply sequential ordering to migrations`
-)
--- migrations/00001_create_books_table.sql
+++ migrations/00001_create_books_table.sql
@@ -1,6 +1,17 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS books
+(
+    id             UUID PRIMARY KEY,
+    title          TEXT      NOT NULL,
+    author         TEXT      NOT NULL,
+    published_date DATE      NOT NULL,
+    image_url      TEXT      NULL,
+    description    TEXT      NULL,
+    created_at     TIMESTAMP NOT NULL,
+    updated_at     TIMESTAMP NOT NULL,
+    deleted_at     TIMESTAMP NULL
+);
 -- +goose StatementEnd
 
 -- +goose Down
